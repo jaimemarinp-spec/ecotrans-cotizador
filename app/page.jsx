71ebb7602b161ec4675f4930d3a72b4252964a19
jaimemarinp-spec <jaimemarinp@gmail.com>
2026-04-18 20:55:08 -0400

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,21 @@
+'use client';
+import React, { useState } from "react";
+
+export default function Page() {
+  const [peso, setPeso] = useState(6000);
+
+  return (
+    <div style={{padding:20}}>
+      <h1>Ecotrans Cotizador</h1>
+      <p>Aplicación funcionando correctamente 🚀</p>
+
+      <input
+        type="number"
+        value={peso}
+        onChange={(e) => setPeso(e.target.value)}
+      />
+
+      <p>Peso ingresado: {peso} kg</p>
+    </div>
+  );
+}
